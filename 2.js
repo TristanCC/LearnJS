1efd476a329299c74c17a44204c92e23859246ef
Tristan Johnston <Tristan.c.johnston@gmail.com>
2023-12-13 00:26:23 -0600

    learning

--- 2.js
+++ 2.js
@@ -1,3 +1,6 @@
+// Object creation and instantiation
+
+// method 1: Class object creation
 class Car {
     constructor(color, wheels, cylinders, size) {
         this.color = color;
@@ -19,10 +22,17 @@ class Car {
     paint(color) {
         this.color = color;
     }
+
+    changeEngine(cylinders, size) {
+        this.engine = {
+            cylinders: cylinders, 
+            size: size
+        }
+    }
 }
 
 const myHonda = new Car("red", 4, 4, 2.2);
-console.log(
+console.log( "myHonda:",
     myHonda.color,
     myHonda.wheels,
     myHonda.engine.cylinders,
@@ -33,9 +43,40 @@ myHonda.drive();
 
 myHonda.paint("blue")
 
-console.log(
+myHonda.changeEngine(6, 3.0);
+
+console.log( "myHonda:",
     myHonda.color,
     myHonda.wheels,
     myHonda.engine.cylinders,
     myHonda.engine.size
 );
+
+// method 2: Object literal creation
+car2 = {
+    color: "blue",
+    wheels: 4,
+    engine: {
+        cylinders: 4,
+        size: 2.2
+    },
+    drive: function() {
+        console.log("Vroom");
+    },
+    brake: function() {
+        console.log("Screech");
+    },
+    paint: function(color) {
+        this.color = color;
+    }
+}
+
+
+// achieves the same as if the function were defined in the literal declaration
+car2.assignColor = function(color) {
+    this.color = color;
+}
+
+car2.assignColor("red");
+
+console.log(car2.color);
---
+++ 3.js
@@ -0,0 +1,38 @@
+// Arrays
+
+
+// 1. Create an array named colors that contains five different names of colors as strings.
+
+const myArray1 = 
+
+// 2. Access the first color in the array and print it to the console using console.log()
+
+// 3.Now do the same with the third color in the list. (Remember that array indexes start at 0, not at 1!)
+
+// 4.Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
+
+// 5. Create a new variable called fourthColor and set it equal to the fourth color in the list.
+
+// 6.Add another color to the end of the list.
+
+// 7.Add another color to the beginning of the list.
+
+// 8. Print the length of the array to the console with console.log()
+
+// 9.Remove the last color from the end of list, and then print the length of the array to the console one more time.
+
+// 10.Write a for loop to iterate through every color in the array and print each color's value to the console.
+
+// 11.Copying from that loop you just wrote, modify it to print every color's value and every color's index in this format: 3, purple or 0, blue etc.
+
+// 12.Create a variable named lastColor that will always point to the last element of the colors array, no matter how many colors are in the list. (Hint: make use of the array's length property for this!)
+
+// 13.Write a function called isLongList that accepts any array as an argument. The isLongList function should return true if the array has more than four elements, or false otherwise. Try testing it with different arrays!
+
+// 14.Write a function called firstAndLast that accepts any array of strings as an argument. The function should return a concatenation of the first element and the last element. Then, try passing any array to the function to test it.
+
+// 15.Write a function called firstAndLength that accepts any array of strings as an argument. The function should return a concatenation of the first element in the array and the length of the first element in the array. If the first element in the array is not a string, return the number 0.
+
+// 16.Write a function called secondAndThird that accepts any array of strings as an argument. The function should return a concatenation of the second and third elements in the array. If the array does not have enough elements, the function should return the message "Not enough words!".
+
+// 17.Write a function called startsWithS that accepts any array of strings as an argument. The function should return true if the array contains any strings that start with the letter "s", and false otherwise. Try calling it with the colors array to test it.
