7957c9f99142fefc3f2b3a2139b4fa9ef5e21fbf
Tristan Johnston <Tristan.c.johnston@gmail.com>
2023-12-13 00:36:17 -0600

    learning

--- 1.js
+++ 1.js
@@ -5,3 +5,4 @@ function helloWorld() {
     console.log(beginnings);
 }
 
+helloWorld();
--- 3.js
+++ 3.js
@@ -3,7 +3,8 @@
 
 // 1. Create an array named colors that contains five different names of colors as strings.
 
-const myArray1 = 
+const myArray1 = ["red", "blue", "greem", "purple", "yellow"]
+console.log(myArray1[0])
 
 // 2. Access the first color in the array and print it to the console using console.log()
 
@@ -37,3 +38,50 @@ const myArray1 =
 
 // 17.Write a function called startsWithS that accepts any array of strings as an argument. The function should return true if the array contains any strings that start with the letter "s", and false otherwise. Try calling it with the colors array to test it.
 
+// 18. Using `map` Method:
+// Write a function that takes an array of numbers and returns a new array with each number doubled. Use the `map` method to achieve this.
+
+// 19. Filtering with `filter` Method:
+// Create a function that accepts an array of strings and returns an array containing only the strings that have more than 3 characters. Use the `filter` method for this task.
+
+// 20. Finding an Element with `find`:
+// Write a function that takes an array of numbers and a number to find. Use the `find` method to return the first element in the array that is greater than the number provided.
+
+// 21. Checking Inclusion with `includes`:
+// Write a function that checks whether an array of strings contains a specific string. Use the `includes` method and return `true` or `false`.
+
+// 22. Copying and Combining Arrays with Spread Operator:
+// Demonstrate how to copy an array and how to combine two arrays into one new array using the spread operator.
+
+// 23. Iterating with `forEach`:
+// Use the `forEach` method to log each element from an array of numbers to the console.
+
+// 24. Iterating with `for...of` Loop:
+// Use a `for...of` loop to iterate over an array of strings and concatenate them into a single string.
+
+// 25. Understanding Array References and Equality:
+// Create two different arrays containing identical strings. Explain why comparing these arrays directly (e.g., `array1 === array2`) returns `false`.
+
+// 26. Summing Numbers with `reduce`:
+// Write a function that calculates the sum of all numbers in an array using the `reduce` method.
+
+// 27. Sorting Arrays:
+// Demonstrate how to sort an array of numbers in ascending order. Then, sort the same array in descending order.
+
+// 28. Custom Sorting with Comparator Function:
+// Create an array of objects where each object has properties `name` and `age`. Write a function to sort the array by age in ascending order using a custom comparator function with the `sort` method.
+
+// 29. Array Destructuring:
+// Given an array of three elements, demonstrate how to use array destructuring to assign each element to a separate variable.
+
+// 30. Multidimensional Arrays:
+// Create a 2D array (an array of arrays) and write a function to print each element on a new line.
+
+// 31. Nested Array Access:
+// Explain how to access the third element of the second array in a 2D array.
+
+// 32. Practical Application - Filtering Elements:
+// Write a function that takes an array of numbers and returns a new array containing only even numbers.
+
+// 33. Error Handling in Array Access:
+// Write a function that tries to access an element at an index that does not exist in the given array. Catch any potential errors and print a custom error message.
